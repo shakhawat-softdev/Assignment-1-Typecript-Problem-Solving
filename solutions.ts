061be922b78f3b6e733e1153d787ed4525153a0b
SM Shakhawat Hosen <45517598+shakhawat-softdev@users.noreply.github.com>
2026-05-07 12:49:10 +0600

    Solved the Problem 3
    Solved the Problem 3

--- solutions.ts
+++ solutions.ts
@@ -7,3 +7,19 @@ const filterEvenNumbers = (numbers: number[]): number[] => {
 const reverseString = (str: string): string => {
   return str.split("").reverse().join("");
 };
+
+//Problem 3:
+type StringOrNumber = string | number;
+
+const checkType = (input: StringOrNumber): string => {
+  if (typeof input === "string") {
+    return "String";
+  } else if (typeof input === "number") {
+    return "Number";
+  }
+
+  throw new Error("Invalid input type");
+};
+
+console.log(checkType("Hello"));
+console.log(checkType(42));
