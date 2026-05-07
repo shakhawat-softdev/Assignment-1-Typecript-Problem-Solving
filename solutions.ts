fe66d7e9c7bea113781c1684b1b6a26d6cc7318a
SM Shakhawat Hosen <45517598+shakhawat-softdev@users.noreply.github.com>
2026-05-07 12:46:17 +0600

    solved the Problem 2

--- solutions.ts
+++ solutions.ts
@@ -2,3 +2,8 @@
 const filterEvenNumbers = (numbers: number[]): number[] => {
   return numbers.filter((num) => num % 2 === 0);
 };
+
+// Problem 2:
+const reverseString = (str: string): string => {
+  return str.split("").reverse().join("");
+};
